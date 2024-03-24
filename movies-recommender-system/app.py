96479741d3884cbb2f39afed19c1054a66e77a14
Neha Shrestha <xtha.nehaa@gmail.com>
2024-03-24 19:26:32 +0545

    UI Poster Added

--- movies-recommender-system/app.py
+++ movies-recommender-system/app.py
@@ -22,7 +22,14 @@ similarity = pickle.load(open('similarity.pkl', 'rb'))
 st.title('Movie Recommendation System')
 selected_movie_name = st.selectbox('Choose your favourite movie.', movies['Series_Title'].values)
 
+
 if st.button('Recommend'):
     recommendations = recommend(selected_movie_name)
-    for i in recommendations:
-        st.write(i)
+    columns = st.columns(len(recommendations))
+    for i, movie_title in enumerate(recommendations):
+        poster_url = movies[movies['Series_Title'] == movie_title]['Poster_Link'].values
+        if poster_url:
+            columns[i].image(poster_url[0], caption=movie_title, width=125)
+
+        else:
+            columns[i].write(f'Poster not available for {movie_title}')
